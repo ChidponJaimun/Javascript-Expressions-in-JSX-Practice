736a0d75b895b686a8593deea4d00468bb5da51e
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2023-07-24 02:35:57 +0700

    ccc ccc

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Chidpon";
+const lName = "Jaimun";
+const date = new Date();
+const year = date.getFullYear();
+ReactDOM.render(
+  <div>
+    <p>Created by {fName + " " + lName}.</p>
+    <p>Copyright {year}.</p>
+  </div>,
+  document.getElementById("root")
+);
